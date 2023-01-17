78ae2aae597c526544f8b6430bdffac512245ba3
kmjang <kmjang@infracube.co.kr>
2023-01-17 13:56:49 +0900

    style: 헤더스타일수정

--- src/styles/mainStyle.js
+++ src/styles/mainStyle.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const Title = styled.h5`
+  font-family: "bsfont-l";
   margin: 0;
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-  font-weight: 400;
+
   font-size: 1.5rem;
   line-height: 1.334;
   letter-spacing: 0em;
@@ -14,7 +14,7 @@ export const ButtonBox = styled.div`
   & > Button {
     border: 1px solid #e1e1e1;
     border-radius: 3px;
-    padding: 10px;
+    padding: 5px 8px;
     background: #f5efef;
     transition: 0.2s all ease-in-out;
   }
