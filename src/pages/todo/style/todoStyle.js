3770b52ab66e2c639b4cb20ddadbccc6d7d34782
kmjang <kmjang@infracube.co.kr>
2023-01-17 15:35:31 +0900

    style : 디테일 스타일 수정

--- src/pages/todo/style/todoStyle.js
+++ src/pages/todo/style/todoStyle.js
@@ -26,3 +26,28 @@ export const List = styled.div`
     width: 10%;
   }
 `;
+
+export const DetailContainer = styled.div`
+  font-family: "bsfont-r";
+  margin: 0;
+  padding: 20px 20px 0 20px;
+  min-height: 200px;
+  font-size: 20px;
+  & input {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+  }
+  & > div:first-child {
+    border-bottom: 1px solid #ddd;
+    padding-bottom: 5px;
+  }
+  & > div:nth-child(2) {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-bottom: 1px solid #ddd;
+    padding-bottom: 20px;
+  }
+  & > div > p.date {
+    text-align: right;
+    font-size: 14px;
+  }
+`;
