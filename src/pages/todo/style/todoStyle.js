65a1cf65233b10907fa79cf402c87eb8decfb5ab
kmjang <kmjang@infracube.co.kr>
2023-01-17 13:57:13 +0900

    style: 리스트 폰트추가

--- src/pages/todo/style/todoStyle.js
+++ src/pages/todo/style/todoStyle.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  font-family: "bsfont-r";
   margin: 0;
   padding: 20px;
   min-height: 500px;
+  & > ul > li {
+    border-bottom: 1px solid #bbb;
+    padding: 10px;
+  }
 `;
 
 export const List = styled.div`
