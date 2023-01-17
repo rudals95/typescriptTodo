e9a01acff0130708b307c6597bffb7728306892e
kmjang <kmjang@infracube.co.kr>
2023-01-17 15:37:41 +0900

    feat : 뒤로가기 추가

--- src/main/components/Header.tsx
+++ src/main/components/Header.tsx
@@ -3,6 +3,7 @@ import { Box } from "@chakra-ui/react";
 import { Title, ButtonBox } from "../../styles/mainStyle";
 import { useNavigate } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
+import { IoIosArrowBack } from "react-icons/io";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -12,6 +13,12 @@ const Header = () => {
   };
   return (
     <Box p="10px" display="flex" justifyContent="space-between" alignItems="center" borderBottom="1.5px solid #e3e0e0">
+      <IoIosArrowBack
+        size={"20px"}
+        onClick={() => {
+          navigate(-1);
+        }}
+      />
       <Title>ToDo</Title>
       <ButtonBox>
         <FiLogOut size="20px" onClick={logOut} />
