b517599bd98044638376ed0996318edf1b0fe1c2
kmjang <kmjang@infracube.co.kr>
2023-01-17 14:00:34 +0900

    feat : 모달 컴포넌트

--- src/main/components/Modal.tsx
+++ src/main/components/Modal.tsx
@@ -3,43 +3,62 @@ import { ITodos } from "../../types/todo";
 
 type Props = {
   isOpen: boolean;
-  onOpen: () => void;
+  onOpen: (btn: string) => void;
   onClose: () => void;
   change: (e: React.ChangeEvent<HTMLInputElement>, type: string) => void;
-  action: () => void;
+  action: (id: any) => void;
   title: string;
   value: ITodos;
+  modalStatus: boolean;
 };
 
-export const IModal: React.FC<Props> = ({ title, value, change, action, isOpen, onOpen, onClose }) => {
+export const IModal: React.FC<Props> = ({ title, value, isOpen, modalStatus, change, action, onOpen, onClose }) => {
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
+      <Button
+        onClick={() => {
+          onOpen("");
+        }}
+      >
+        Open Modal
+      </Button>
 
       <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{title}</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <Input
-              value={value.title}
-              onChange={(e) => {
-                change(e, "title");
-              }}
-              mb="10px"
-              placeholder="제목"
-            />
-            <Input
-              value={value.content}
-              onChange={(e) => {
-                change(e, "content");
-              }}
-              placeholder="내용"
-            />
-          </ModalBody>
+          {modalStatus ? (
+            <ModalBody>삭제하시겠습니까?</ModalBody>
+          ) : (
+            <ModalBody>
+              <Input
+                value={value.title}
+                onChange={(e) => {
+                  change(e, "title");
+                }}
+                mb="10px"
+                placeholder="제목"
+              />
+              <Input
+                value={value.content}
+                onChange={(e) => {
+                  change(e, "content");
+                }}
+                placeholder="내용"
+              />
+            </ModalBody>
+          )}
+
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={action}>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={action}
+              // onClick={() => {
+              //   action("delete");
+              // }}
+            >
               Save
             </Button>
             <Button colorScheme="red" onClick={onClose}>
